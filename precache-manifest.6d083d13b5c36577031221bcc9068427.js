1ae674c5f9fabcf3fdc99a9af9ce0026ea412d52
loki2k <jeremylagarto@gmail.com>
2020-12-13 11:07:15 -0800

    Updates

---
+++ precache-manifest.6d083d13b5c36577031221bcc9068427.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e9719f1a1244add66c8c5373ae6cb6",
+    "url": "/cocktails/index.html"
+  },
+  {
+    "revision": "539b74281fe553b4e944",
+    "url": "/cocktails/static/css/main.db6e84ad.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/cocktails/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "539b74281fe553b4e944",
+    "url": "/cocktails/static/js/main.bd32830a.chunk.js"
+  },
+  {
+    "revision": "e448c4ac95b4b7fbb491",
+    "url": "/cocktails/static/js/runtime-main.42b4d6f2.js"
+  },
+  {
+    "revision": "f3ad766494d1f9701717c1a860cd5b10",
+    "url": "/cocktails/static/media/logo.f3ad7664.svg"
+  }
+]);
